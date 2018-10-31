1adc1c72c64d2ebab285d71a1f0795a562897fc3
Wexxion <Wexxion@gmail.com>
2018-11-01 01:48:04 +0500

    Done

--- emitter.js
+++ emitter.js
@@ -6,11 +6,39 @@
  */
 const isStar = true;
 
+
+function handleEvent(contexts) {
+    for (let [obj, handlers] of contexts) {
+        for (let handler of handlers) {
+            execFunc(obj, handler);
+        }
+    }
+}
+
+function execFunc(obj, handler) {
+    let shouldCall = true;
+    if (handler.times !== 0 && handler.calls >= handler.times) {
+        shouldCall = false;
+    }
+
+    if (handler.frequency !== 0 && handler.calls % handler.frequency !== 0) {
+        shouldCall = false;
+    }
+
+    if (shouldCall) {
+        handler.func.call(obj);
+    }
+
+    handler.calls++;
+}
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const events = new Map();
+
     return {
 
         /**
@@ -18,26 +46,70 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @param {Object} additional
+         * @returns {Object}
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on: function (event, context, handler, additional = { times: 0, frequency: 0 }) {
+            if (!events.has(event)) {
+                events.set(event, new Map());
+            }
+
+            const contexts = events.get(event);
+
+            if (!contexts.has(context)) {
+                contexts.set(context, []);
+            }
+
+            contexts.get(context).push({
+                func: handler,
+                times: additional.times,
+                frequency: additional.frequency,
+                calls: 0
+            });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (let eventName of events.keys()) {
+                if (eventName === event || eventName.startsWith(event + '.')) {
+                    events.get(eventName).delete(context);
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            const allEvents = [];
+            let prev = '';
+
+            for (let part of event.split('.')) {
+                if (prev !== '') {
+                    prev += '.';
+                }
+
+                allEvents.push(prev += part);
+            }
+
+            for (let cuurEvent of allEvents.reverse()) {
+                if (events.has(cuurEvent)) {
+                    handleEvent(events.get(cuurEvent));
+                }
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +119,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            this.on(event, context, handler, { times, frequency: 0 });
+
+            return this;
         },
 
         /**
@@ -59,9 +134,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            this.on(event, context, handler, { times: 0, frequency });
+
+            return this;
         }
     };
 }
